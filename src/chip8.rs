434b8ea482dcc4109a1149d0c4c46dff33d324d2
siddh-m <henry-sm>
2024-06-24 23:58:46 +0530

    minor changes

--- src/chip8.rs
+++ src/chip8.rs
@@ -1,5 +1,6 @@
 use rand::Rng;
 
+
 const FONT: [[u8; 5]; 16] = [
     [0xf0, 0x90, 0x90, 0x90, 0xf0], // 0, 1 all the way to F
     [0x20, 0x60, 0x20, 0x20, 0x70],
@@ -19,7 +20,7 @@ const FONT: [[u8; 5]; 16] = [
     [0xf0, 0x80, 0xf0, 0x80, 0x80], 
 ];
 
-struct Chip8 {
+pub struct Chip8 {
     register : [u8; 16], //16 general purpose registers
     i : u16, //store mem adress
     dt : u8, //delay itmer
@@ -30,20 +31,36 @@ struct Chip8 {
     memory : [u8; 0xfff], // 4k RAM
     display : [[u8;64];32], // 64x32 display
     keypad : [bool; 16], // keypad input
+    file : std::fs::File,
 }
 
 
 impl Chip8{
     
+    pub fn new(chfile :std::fs::File ) -> Chip8{
+        let mut memory = [0; 0xfff];
+        memory[..80].copy_from_slice(&FONT[0]);
+        Chip8{
+            register : [0; 16],
+            i : 0,
+            dt : 0,
+            st : 0,
+            pc : 0x200,
+            stk : Vec::new(),
+            memory : memory,
+            display : [[0;64];32],
+            keypad : [false; 16],
+            file : chfile,
+        }
+    }
 
 
-
-    fn CPU_reset(&mut self){
+    pub fn CPU_reset(&mut self){
         self.pc = 0x200;
         //memset (register, 0, std::mem::size_of_val(&register)); //registers are 0 //where did memset go?
     }
 
-    fn opcode(&mut self){
+    pub fn opcode(&mut self){
         //take opcode from memory and read 2 bytes from it
         let opcode: u16 = ((self.memory[self.pc as usize] as u16) << 8 | (self.memory[(self.pc + 1) as usize] as u16));
         let nnn: u16 = opcode & 0xfff;
@@ -109,70 +126,85 @@ impl Chip8{
         }
     }
 
+    pub fn clock_cycle(&mut self){
+        self.opcode(); //fetch opcode
+        self.pc += 2; //increment program counter by 2 bytes
+        if self.dt > 0{ 
+            self.dt -= 1;
+        }
+        if self.st > 0{
+            self.st -= 1;
+        }
+        //decrement the timers
+
+
+    
+    }
+
 }
 
 impl Chip8{ //separate impl for opcodes 
 
-    fn _00e0(&mut self){
+    pub fn _00e0(&mut self){
         self.display.fill([0;64]);
     }
 
-    fn _00ee(&mut self){
+    pub fn _00ee(&mut self){
         self.pc = self.stk.pop().unwrap();
     }
 
-    fn _1nnn(&mut self, nnn : u16){
+    pub fn _1nnn(&mut self, nnn : u16){
         self.pc = nnn;
     }
 
-    fn _2nnn(&mut self, nnn : u16){
+    pub fn _2nnn(&mut self, nnn : u16){
         self.stk.push(self.pc);
         self.pc = nnn;
     }
 
-    fn _3xkk(&mut self, x : usize, kk:u8){
+    pub fn _3xkk(&mut self, x : usize, kk:u8){
         if self.register[x] == kk{
             self.pc += 2;
         }
     }
 
-    fn _4xkk(&mut self, x:usize, kk:u8){
+    pub fn _4xkk(&mut self, x:usize, kk:u8){
         if self.register[x] != kk{
             self.pc += 2;
         }
     }
 
-    fn _5xy0(&mut self, x:usize, y:usize){
+    pub fn _5xy0(&mut self, x:usize, y:usize){
         if self.register[x] == self.register[y]{
             self.pc += 2;
         }
     }
 
-    fn _6xkk(&mut self, x:usize, kk:u8){
+    pub fn _6xkk(&mut self, x:usize, kk:u8){
         self.register[x] = kk;
     }
 
-    fn _7xkk(&mut self, x:usize, kk:u8){
+    pub fn _7xkk(&mut self, x:usize, kk:u8){
         self.register[x] += kk;
     }
 
-    fn _8xy0(&mut self, x:usize, y:usize){
+    pub fn _8xy0(&mut self, x:usize, y:usize){
         self.register[x] = self.register[y];
     }
 
-    fn _8xy1(&mut self, x:usize, y:usize){
+    pub fn _8xy1(&mut self, x:usize, y:usize){
         self.register[x] |= self.register[y];
     }
 
-    fn _8xy2(&mut self, x:usize, y:usize){
+    pub fn _8xy2(&mut self, x:usize, y:usize){
         self.register[x] &= self.register[y];
     }
 
-    fn _8xy3(&mut self, x:usize, y:usize){
+    pub fn _8xy3(&mut self, x:usize, y:usize){
         self.register[x] ^= self.register[y];
     }
 
-    fn _8xy4(&mut self, x:usize, y:usize){
+    pub fn _8xy4(&mut self, x:usize, y:usize){
         if self.register[x] + self.register[y] > 0xff{
             self.register[0xf] = 1;
         }
@@ -180,44 +212,44 @@ impl Chip8{ //separate impl for opcodes
         self.register[x] = (self.register[x] + self.register[y]) & 0xff;
     }
 
-    fn _8xy5(&mut self, x:usize, y:usize){
+    pub fn _8xy5(&mut self, x:usize, y:usize){
         self.register[0xf] = if self.register[x] > self.register[y]{1} else {0};
         self.register[x] -= self.register[y];
     }
 
-    fn _8xy6(&mut self, x:usize, y:usize){
+    pub fn _8xy6(&mut self, x:usize, y:usize){
         self.register[0xf] = if self.register[x] & 0x1 == 1{1} else {0};
         self.register[x] >>= 1; //divided by 2
     }
 
-    fn _8xy7(&mut self, x:usize, y:usize){
+    pub fn _8xy7(&mut self, x:usize, y:usize){
         self.register[0xf] = if self.register[y] > self.register[x]{1} else {0};
         self.register[x] = self.register[y] - self.register[x];
     }
 
-    fn _8xye(&mut self, x:usize, y:usize){
+    pub fn _8xye(&mut self, x:usize, y:usize){
         self.register[0xf] = if self.register[x] & 0x80 == 0x80{1} else {0};
         self.register[x] <<= 1; //multiply by 2
     }
 
-    fn _9xy0(&mut self, x:usize, y:usize){
+    pub fn _9xy0(&mut self, x:usize, y:usize){
         self.pc+= if self.register[x] != self.register[y]{2} else {0};
     }
 
-    fn _annn(&mut self, nnn:u16){
+    pub fn _annn(&mut self, nnn:u16){
         self.i = nnn;
     }
 
-    fn _bnnn(&mut self, nnn:u16){
+    pub fn _bnnn(&mut self, nnn:u16){
         self.pc = (nnn + (self.register[0] as u16)) as u16;
     }
 
-    fn _cxkk(&mut self, x:usize, kk:u8){
+    pub fn _cxkk(&mut self, x:usize, kk:u8){
         self.register[x] = rand::thread_rng().gen_range(0..255) & kk;
     }
 
     //not confident on this opcode
-    fn _dxyn(&mut self, x:usize, y:usize, n:u8){
+    pub fn _dxyn(&mut self, x:usize, y:usize, n:u8){
         let mut pixel;
         self.register[0xf] = 0;
         for vy in 0..n{
@@ -236,19 +268,19 @@ impl Chip8{ //separate impl for opcodes
         }
     }
 
-    fn _ex9e(&mut self, x:usize){
+    pub fn _ex9e(&mut self, x:usize){
         self.pc += if self.keypad[self.register[x] as usize]{2} else {0};
     }
 
-    fn _exa1(&mut self, x:usize){
+    pub fn _exa1(&mut self, x:usize){
         self.pc += if !self.keypad[self.register[x] as usize]{2} else {0};
     }
 
-    fn _fx07(&mut self, x:usize){
+    pub fn _fx07(&mut self, x:usize){
         self.register[x] = self.dt;
     }
 
-    fn _fx0a(&mut self, x:usize){
+    pub fn _fx0a(&mut self, x:usize){
         for i in 0..16{
             if self.keypad[i]{
                 self.register[x] = i as u8;
@@ -256,35 +288,35 @@ impl Chip8{ //separate impl for opcodes
         }
     }
 
-    fn _fx15(&mut self, x:usize){
+    pub fn _fx15(&mut self, x:usize){
         self.dt = self.register[x];
     }
 
-    fn _fx18(&mut self, x:usize){
+    pub fn _fx18(&mut self, x:usize){
         self.st = self.register[x];
     }
 
-    fn _fx1e(&mut self, x:usize){
+    pub fn _fx1e(&mut self, x:usize){
         self.i += self.register[x] as u16;
     }
 
-    fn _fx29(&mut self, x:usize){
+    pub fn _fx29(&mut self, x:usize){
         self.i = (self.register[x] as u16) * 5; //sprites are 5 bytes
     }
 
-    fn _fx33(&mut self, x:usize){
+    pub fn _fx33(&mut self, x:usize){
         self.memory[self.i as usize] = self.register[x] / 100;
         self.memory[(self.i + 1) as usize] = (self.register[x] / 10) % 10;
         self.memory[(self.i + 2) as usize] = self.register[x] % 10;
     }
 
-    fn _fx55(&mut self, x:usize){
+    pub fn _fx55(&mut self, x:usize){
         for q in 0..x{
             self.memory[self.i as usize + q] = self.register[q];
         }
     }
 
-    fn _fx65(&mut self, x:usize){
+    pub fn _fx65(&mut self, x:usize){
         for q in 0..x{
             self.register[q] = self.memory[self.i as usize + q];
         }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,23 @@
 use chip8emu::chip8;
+use std::fs::File;
+use std::env;
+use std::io::Read;
 
+fn main() -> std::io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        eprintln!("Usage: program_name file.ch8");
+        return Err(std::io::Error::new(std::io::ErrorKind::InvalidInput, "No file specified"));
+    }
 
-fn main() {
+    let ch8filename = &args[1];
+    let mut ch8file = File::open(ch8filename)?; // Handle the Result using `?`
+
+    let mut CHIP8 = chip8::Chip8::new(ch8file);
+    CHIP8.CPU_reset();
+    CHIP8.opcode();
     println!("Hello, world!");
+    Ok(())
 }
 
 
